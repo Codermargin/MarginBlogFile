f305a65ef31771b18fecc793e359c4606b54180b
EvolutionMar <1273930793@qq.com>
2020-09-04 10:42:06 +0800

    第一次进行提交

---
+++ GitTestProject/src/main/java/com/xyx/nihao.java
@@ -0,0 +1,18 @@
+package com.xyx;
+
+public class nihao {
+    public static void main(String[] args) {
+        System.out.println("hello project");
+        System.out.println();
+        System.out.println("hello project");
+        System.out.println();
+        System.out.println("hello project");
+        System.out.println();
+        System.out.println("hello project");
+        System.out.println();
+        System.out.println("hello project");
+        System.out.println();
+        System.out.println("hello project");
+        System.out.println();
+    }
+}
